2979f2048ecd1ac142f40c39f0e3eefc19cde640
Ngoc Hieu Nguyen <ngochieua2@gmail.com>
2020-12-28 15:13:52 +1030

    create input component

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import classes from './Input.module.css';
+
+const Input = (props) => {
+    let inputElement = null;
+    let error = null;
+    const inputClasses = [classes.InputElement];
+
+    if (!props.valid && props.touched){
+        inputClasses.push(classes.Invalid);
+        error = <p className={classes.Error}>Please enter a valid value!</p>;
+    }
+
+    switch (props.elementType) {
+        case ('input'):
+            inputElement = <input 
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value} 
+                onChange={props.changed} />
+            break;
+        //More type...
+        default:
+            inputElement = <input 
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value} 
+                onChange={props.changed} />
+            break;
+    }
+
+    return (
+        <div className={classes.Input} >
+            <label className={classes.Label}>{props.label}</label>
+            {inputElement}
+            {/* {error} */}
+            
+        </div>
+    )
+}
+
+export default Input;
