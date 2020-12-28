c5f14904d84eaa66fca11018ee70c9ab2d93ceda
Ngoc Hieu Nguyen <ngochieua2@gmail.com>
2020-12-29 01:55:59 +1030

    set up router to the whole project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './containers/Layout/Layout';
 import Login from './containers/LoginPage/Login';
+import HomePage from './containers/HomePage/HomePage';
 
 class App extends Component {
     render () {
         return (
             <Layout>
-                <Login />
+                <Switch>
+                    <Route path="/" exact component={Login} />
+                    <Route path="/homePage" component={HomePage} />
+                </Switch>
             </Layout>
         )
     }
