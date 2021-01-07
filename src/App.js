0949a64bc84813e5cb996e4f0b3b530c57001e2f
Ngoc Hieu Nguyen <ngochieua2@gmail.com>
2021-01-07 23:39:15 +1030

    change to react hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Layout from './containers/Layout/Layout';
 import Login from './containers/LoginPage/Login';
 
-class App extends Component {
-    render () {
-        return (
-            <Layout>
-                <Login />
-            </Layout>
-        )
-    }
+const App = props =>  {
+    return (
+        <Layout>
+            <Login />
+        </Layout>
+    )
 }
 
 export default App;
--- src/containers/LoginPage/Login.js
+++ src/containers/LoginPage/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import classes from './Login.module.css';
 import Logo from '../../components/Logo/Logo';
@@ -6,118 +6,111 @@ import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import SocialImage from '../../components/SocialImage/SocialImage';
 
-class Login extends Component {
-    state = {
-        form: {
-            Username: {
-                label: 'Username: ',
-                elementType: 'input',
-                elementConfig: {
-                    text: 'email',
-                    placeholder: 'Please enter your username',
-                },
-                value: '',
-                validation: {
-                    required: true,
-                },
-                valid: false,
-                touched: false,
+const Login = props => {
+    const [formStructure, setFormStructure] = useState({
+        Username: {
+            label: 'Username: ',
+            elementType: 'input',
+            elementConfig: {
+                text: 'email',
+                placeholder: 'Please enter your username',
             },
-            Password: {
-                label: 'Password: ',
-                elementType: 'input',
-                elementConfig: {
-                    text: 'password',
-                    placeholder: 'Please enter your password',
-                },
-                value: '',
-                validation: {
-                    required: true,
-                },
-                valid: false,
-                touched: false,
+            value: '',
+            validation: {
+                required: true,
             },
+            valid: false,
+            touched: false,
         },
-        formIsValid: false,
-    }
+        Password: {
+            label: 'Password: ',
+            elementType: 'input',
+            elementConfig: {
+                text: 'password',
+                placeholder: 'Please enter your password',
+            },
+            value: '',
+            validation: {
+                required: true,
+            },
+            valid: false,
+            touched: false,
+        },
+    });
+    const [formIsValid, setFormIsValid] = useState(false);
 
-    checkValidity (value, rules) {
+    const checkValidity = (value, rules) => {
         let isValid = true;
 
         if (rules.required){
             isValid = value.trim() !== '' && isValid;
         }
-
         //More rules here
 
         return isValid;
     }
 
-    inputChangedHandler = (event, inputIdenfifier) =>{
-        const updatedForm = {...this.state.form};
+    const inputChangedHandler = (event, inputIdenfifier) => {
+        const updatedForm = {...formStructure};
         const updatedFormElement = {...updatedForm[inputIdenfifier]};
         updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.valid = checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedFormElement.touched = true;
         updatedForm[inputIdenfifier] = updatedFormElement;
 
         // Check validity of all element
-        let formIsValid = true;
+        let checkFormIsValid = true;
         for(let Identifier in updatedForm) {
-            formIsValid = updatedForm[Identifier].valid && formIsValid;
+            checkFormIsValid = updatedForm[Identifier].valid && checkFormIsValid;
         }
-
         // update state
-        this.setState({form: updatedForm, formIsValid: formIsValid});
-    
+        setFormStructure(updatedForm);
+        setFormIsValid(checkFormIsValid);
     }
 
-    render () {
-        //get form info in state
-        const formElementArray = [];
-        for (let element in this.state.form){
-            formElementArray.push({
-                id: element,
-                config: this.state.form[element]
-            });
-        }
+    //get form info in state
+    const formElementArray = [];
+    for (let element in formStructure){
+        formElementArray.push({
+            id: element,
+            config: formStructure[element]
+        });
+    }
 
-        //create form components
-        let form = (
-            <form onSubmit="/">
-                {formElementArray.map(element => (
-                    <Input
-                        key={element.id}
-                        label={element.config.label}
-                        elementType={element.config.elementType}
-                        elementConfig={element.config.elementConfig}
-                        value={element.config.value}
-                        valid={element.config.valid}
-                        // shouldValidation={element.config.validation}
-                        touched={element.config.touched}
-                        changed={(event) => this.inputChangedHandler(event, element.id)} />
-                ))}
-                <div className={classes.Link}><Button btnType="Link">Forgot Password</Button></div>
-                <Button btnType="Success" disabled={!this.state.formIsValid}>LOGIN</Button>
-            </form>
-        );
+    //create form components
+    let form = (
+        <form onSubmit="/">
+            {formElementArray.map(element => (
+                <Input
+                    key={element.id}
+                    label={element.config.label}
+                    elementType={element.config.elementType}
+                    elementConfig={element.config.elementConfig}
+                    value={element.config.value}
+                    valid={element.config.valid}
+                    touched={element.config.touched}
+                    changed={(event) => inputChangedHandler(event, element.id)} />
+            ))}
+            <div className={classes.Link}><Button btnType="Link">Forgot Password</Button></div>
+            <Button btnType="Success" disabled={!formIsValid}>LOGIN</Button>
+        </form>
+    );
 
-        return (
-            <React.Fragment>
-                <Logo />
-                <div className={classes.Form}>
-                    {form}
-                </div>
-                <div className={classes.Or}>OR</div>
-                <div className={classes.Register}><Button btnType="Link">Register here</Button> or use social media</div>
-                <div className={classes.Social}>
-                    <Button btnType="Social" disabled={false}><SocialImage ImageType="facebook" /></Button>
-                    <Button btnType="Social" disabled={false}><SocialImage ImageType="twitter" /></Button>
-                    <Button btnType="Social" disabled={false}><SocialImage ImageType="google" /></Button>
-                </div>
-            </React.Fragment>
-        );
-    };
+    return (
+        <React.Fragment>
+            <Logo />
+            <div className={classes.Form}>
+                {form}
+            </div>
+            <div className={classes.Or}>OR</div>
+            <div className={classes.Register}><Button btnType="Link">Register here</Button> or use social media</div>
+            <div className={classes.Social}>
+                <Button btnType="Social" disabled={false}><SocialImage ImageType="facebook" /></Button>
+                <Button btnType="Social" disabled={false}><SocialImage ImageType="twitter" /></Button>
+                <Button btnType="Social" disabled={false}><SocialImage ImageType="google" /></Button>
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default Login;
