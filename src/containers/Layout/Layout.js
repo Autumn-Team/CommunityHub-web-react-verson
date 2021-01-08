3993cef982d5ab4052be0d9f8c57a381cd996b97
Ngoc Hieu Nguyen <ngochieua2@gmail.com>
2021-01-09 02:58:00 +1030

    add navBar into layout after login

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 import Footer from '../../components/Footer/Footer';
+import NavBar from '../../components/Navigation/NavBar/NavBar';
 
 const Layout = props =>  {
     const location = useLocation();
-    let bar = null;
-    if(location.pathname === "/homePage"){
-        bar = <div>Something here</div>;
+    let toolBar = null;
+    if(location.pathname !== "/"){
+        toolBar = <NavBar />;
     }
 
     return (
         <React.Fragment>
+            {toolBar}
             <main >
-                {bar}
                 {props.children}
             </main>
             <Footer />
