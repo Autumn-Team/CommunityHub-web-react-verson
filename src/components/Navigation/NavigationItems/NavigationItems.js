45f9a0559aae715b1e924518cbe4699d5bf73030
Ngoc Hieu Nguyen <ngochieua2@gmail.com>
2021-01-09 02:57:03 +1030

    implement all navigationItems with simple css

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './NavigationItems.module.css';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const NavigationBar = () => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/homePage" exact>HomePage</NavigationItem>
+        <NavigationItem link="/forum">Forum</NavigationItem>
+        <NavigationItem link="/event">Event</NavigationItem>
+        <NavigationItem link="/market">Market</NavigationItem>
+        <NavigationItem link="/message">Message</NavigationItem>
+    </ul>
+);
+
+export default NavigationBar;
