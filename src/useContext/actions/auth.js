d62f99c399a04d3321ccff7b1e53affa60e4097c
Ngoc Hieu Nguyen <ngochieua2@gmail.com>
2021-01-29 15:07:29 +1030

    done in auth actions

--- src/useContext/actions/auth.js
+++ src/useContext/actions/auth.js
@@ -2,12 +2,58 @@ import { login, register } from '../../axios-instance';
 
 import * as actionTypes from './actionTypes';
 
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START,
+    }
+}
+
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        token: token,
+        userId: userId,
+    }
+}
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error,
+    }
+}
+
 export const authLogin = (dispatch, email, password) => {
-    dispatch({type: actionTypes.AUTH_START});
+    dispatch(authStart());
     const loginData = {
         email: email,
         password: password,
         returnSecureToken: true,
     }
-}
+
+    login.post('', loginData)
+        .then(response => {
+            dispatch(authSuccess(response.data.idToken, response.data.localId));
+        })
+        .catch(error => {
+            dispatch(authFail(error.response.data.error));
+        })
+};
+
+export const authRegister = (dispatch, email, password) => {
+    dispatch(authStart());
+    const registerData = {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+    }
+
+    register.post('', registerData)
+        .then(response => {
+            dispatch(authSuccess(response.data.idToken, response.data.localId));
+        })
+        .catch(error => {
+            dispatch(authFail(error.response.data.error));
+        })
+};
 
