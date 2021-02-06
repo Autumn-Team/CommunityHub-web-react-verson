75f9d730817b953d51c4c183a5316d6d759c26b4
Le Truong Thanh <thanhpham021299@gmail.com>
2021-02-06 23:51:22 +1030

    Reset css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
-
 import { Route, Switch } from 'react-router-dom';
 
 import Layout from './containers/Layout/Layout';
 import Login from './containers/LoginPage/Login';
+import ResetPassword from './containers/ResetPage/ResetPassword';
 import HomePage from './containers/HomePage/HomePage';
 import Event from './containers/EventFeature/MainEventPage/MainEventPage';
 import NewEvent from './containers/EventFeature/NewEvent/NewEvent';
 import EventDetails from './containers/EventFeature/EventDetails/EventDetails';
-import Register from './containers/RegisterPage/registerPage';
 
 const App = props =>  {
     return (
         <Layout>
             <Switch>
                 <Route path="/" exact component={Login} />
-                <Route path="/register" component={Register} />
+                <Route path="/resetPassword" component={ResetPassword} />
                 <Route path="/homePage" component={HomePage} />
                 <Route path="/event" exact component={Event} />
                 <Route path="/event/newEvent" component={NewEvent} />
---
+++ src/containers/ResetPage/ResetPassword.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+
+import classes from './Reset.module.css';
+import Logo from '../../components/Logo/Logo';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+import { checkValidity, updateObject } from '../../sharedFunctions/utility';
+
+const Reset = props =>{
+    const[resetForm,setResetForm] = useState({
+        NewPassword: {
+            label:'New password: ',
+            elementType: 'input',
+            elementConfig:{
+                placeholder: 'Please enter your new password: ',
+                type:  'password',
+
+            },
+            value : '',
+            validation: {
+                required: true
+            },
+            valid: false,
+            touched: false,
+        },
+        ConfirmPassword: {
+            label:'Confirm password: ',
+            elementType: 'input',
+            elementConfig:{
+                placeholder: "Please confirm your new password: ",
+                type:  'password',
+
+            },
+            value : '',
+            validation: {
+                required: true
+            },
+            valid: false,
+            touched: false,
+        },
+    });
+    const[formValidation, setFormIsValid] = useState(false);
+
+    const inputChangedHandler = (event, inputIdentifier) =>{
+        const updatedFormElement= updateObject(resetForm[inputIdentifier],{
+            value: event.target.value,
+            valid: checkValidity(event.target.value, resetForm[inputIdentifier].validation),
+            touched: true,
+        })
+
+        const updatedResetForm = updateObject(resetForm, {
+            [inputIdentifier] : updatedFormElement,
+        })
+        //CHECK validity of all elements
+        let checkFormIsValid = true;
+        for(let Identifier in updatedResetForm){
+           checkFormIsValid =  updatedResetForm[Identifier].valid && checkFormIsValid;
+        }
+        setResetForm(updatedResetForm);
+        setFormIsValid(checkFormIsValid);
+    }
+    const resetEventHandler = (event) => {
+        event.preventDefault();
+        //handle event
+        props.history.push('/homePage');
+    }
+
+    //get form info in state
+    const formElementArray = [];
+    for (let element in resetForm){
+        formElementArray.push({
+            id: element,
+            config: resetForm[element]
+        });
+    }
+
+    //create form components
+    let form = (
+        <form onSubmit={resetEventHandler}>
+            {formElementArray.map(element => (
+                <Input
+                    key={element.id}
+                    label={element.config.label}
+                    elementType={element.config.elementType}
+                    elementConfig={element.config.elementConfig}
+                    value={element.config.value}
+                    valid={element.config.valid}
+                    touched={element.config.touched}
+                    changed={(event) => inputChangedHandler(event, element.id)} />
+            ))}
+            <Button btnType="Success" disabled={!formValidation}>Change password</Button>
+        </form>
+    );
+    return (
+        <React.Fragment>
+            <Logo />
+            <div className={classes.Form}>
+                {form}
+            </div>
+            
+        </React.Fragment>
+    );
+}
+export default Reset;
