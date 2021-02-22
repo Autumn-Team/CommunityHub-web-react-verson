730f14ad884e4cc21338809543d4a370023d2b1b
Le Truong Thanh <thanhpham021299@gmail.com>
2021-02-23 00:21:23 +1030

    Finish main forum page, need to test need to fix bugs

--- src/containers/ForumFeature/MainForumPage/MainForum.js
+++ src/containers/ForumFeature/MainForumPage/MainForum.js
@@ -15,33 +15,33 @@ const MainForumPage = props =>{
         onFetchEvents();
     }, [onFetchEvents, location]);
     let h2Title = null;
-    let events = null;
-    let eventType = null;
+    let forums = null;
+    let forumType = null;
     let sideNav = null;
     switch (location.pathname) {
         case "/forum":
             h2Title = <h2>Next Forum</h2>;
             sideNav = <SideNavBar featureType="MainForum" />;
-            events = <Spinner />;
-            eventType = "forum";
+            forums = <Spinner />;
+            forumType = "forum";
             break;
         case "/forum/enteredEvent":
             h2Title = <h2>Attending Events List</h2>;
             sideNav = <SideNavBar featureType="enteredEvent" />;
-            events = <Spinner />;
-            eventType = "enterdForum";
+            forums = <Spinner />;
+            forumType = "enterdForum";
             break;
         case "/event/yourForum":
             h2Title = <h2>Your Created Forum</h2>;
             sideNav = <SideNavBar featureType="yourForum" />;
-            events = <Spinner />;
-            eventType = "yourForum";
+            forums = <Spinner />;
+            forumType = "yourForum";
             break;
         default:
             h2Title = null;
             sideNav = null;
-            events = null;
-            eventType = null;
+            forums = null;
+            forumType = null;
             break;
     }
 
@@ -49,20 +49,20 @@ const MainForumPage = props =>{
         props.history.push('/forum/' + forumId);
     }
 
-
+    ////
     if (!props.loading) {
         events = props.error ? 
             (<p className={classes.errMessage}>{props.error.message}</p>) : 
-            (props.events.map(currentEvent => (  
-                <Event key={currentEvent.id} 
-                date={currentEvent.eventData.date} 
-                title={currentEvent.eventData.title} 
-                location={currentEvent.eventData.location}
-                eventType={eventType}
-                seeMoreClicked= {() => seeMoreClickedHandler(currentEvent.id)} />
+            (props.events.map(currentForum => (  
+                <Event key={currentForum.id} 
+                date={currentForum.eventData.date} 
+                title={currentForum.eventData.title} 
+                location={currentForum.eventData.location}
+                forumType={forumType}
+                seeMoreClicked= {() => seeMoreClickedHandler(currentForum.id)} />
             )));
         
-        if (events.length === 0) events = <p className={classes.Empty}>There is no event at the moment</p>;  
+        if (events.length === 0) events = <p className={classes.Empty}>There is no forum post at the moment</p>;  
     }
 
     return (
